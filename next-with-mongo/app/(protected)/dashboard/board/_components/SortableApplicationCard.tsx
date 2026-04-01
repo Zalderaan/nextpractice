d8d403f4f5756d32d74043653915d4abc2a84397
Godfrey Eclarinal <eclarinalgodfrey@gmail.com>
2026-04-01 10:47:23 +0800

    installed dnd-kit modules pre-refactoring

---
+++ next-with-mongo/app/(protected)/dashboard/board/_components/SortableApplicationCard.tsx
@@ -0,0 +1,44 @@
+import React from 'react'; // remove unused "use"
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import { ApplicationCard, Application } from './ApplicationCard';
+
+interface SortableApplicationCardProps {
+    application: Application;
+    onClick: () => void;
+}
+
+export function SortableApplicationCard({ application, onClick }: SortableApplicationCardProps) {
+    const {
+        attributes,
+        listeners,
+        setNodeRef,
+        transform,
+        transition,
+        isDragging,
+    } = useSortable({
+        id: application._id,
+        data: {
+            type: "Application",
+            status: application.status
+        }
+    });
+
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+        opacity: isDragging ? 0 : 1, // key change
+    };
+
+    return (
+        <div
+            ref={setNodeRef}
+            style={style}
+            {...attributes}
+            {...listeners}
+            data-dnd-handle="true"
+        >
+            <ApplicationCard application={application} onClick={onClick} />
+        </div>
+    )
+}
